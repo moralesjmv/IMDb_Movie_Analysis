112bad7ec58b1889f873eb5cb9aba8fe75a7c5d3
moralesjmv <jmorales.mv@gmail.com>
2018-06-26 18:39:38 -0700

    Initial commit

---
+++ gather_movie_data.py
@@ -0,0 +1,26 @@
+import imdb
+
+def store_movie_data(dest):
+    """
+    Stores movie titles and summaries into a file since doing IMDb calls take too long
+    """
+
+    top_movies = open("./movie_ids_top_250.txt", 'r').read()
+    bottom_movies = open("./movie_ids_bottom_250.txt", 'r').read()
+
+    ia = imdb.IMDb()
+    summaries = []
+    n = 0
+    for line in top_movies.strip().split('\n'):
+        print("Progress: " + str(n) + "%")
+        n += 1
+        movie = ia.get_movie(line)
+        summaries.append(str(movie['year']))
+        #print(movie['year'])
+    joined_lines = '\n'.join(summaries)
+    open(dest,'w').write(joined_lines)
+
+top_summaries = []
+bottom_summaries = []
+store_movie_data("./movie_years_top_250.txt")
+#load_data_into_array(top_summaries, bottom_summaries)
